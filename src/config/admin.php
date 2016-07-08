0500f4020b67215693875ea2b2eef335a8ddf03c
lembarek <lembarek2011@gmail.com>
2016-07-08 21:27:47 +0100

    add roles, tags, to sidebar in admin

--- src/config/admin.php
+++ src/config/admin.php
@@ -2,9 +2,11 @@
 
 return [
     "links" => [
-        ["/","home"],
+        ["/dashboard","home"],
         ["/dashboard/users","users"],
         ["/dashboard/posts","posts"],
+        ["/dashboard/roles","roles"],
+        ["/dashboard/tags","tags"],
     ],
 
     'paginate' => 22,
