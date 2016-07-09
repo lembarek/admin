62e11347fca7adf2b389040906baa68f264ad822
lembarek <lembarek2011@gmail.com>
2016-07-09 16:55:12 +0100

    fix small bug

--- src/config/admin.php
+++ src/config/admin.php
@@ -8,7 +8,7 @@ return [
         ["admin::dashboard.roles.index", '', "roles"],
         ["admin::dashboard.tags.index", '', "tags"],
         ["admin::dashboard.categories.index", '', "categories"],
-        ["admin::dashboard.users.show", '["username" => $user->username]', "settings"],
+        ["admin::dashboard.users.show", '["username" => auth()->user()->username]', "settings"],
     ],
 
     'paginate' => 22,
