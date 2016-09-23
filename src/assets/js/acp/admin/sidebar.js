7292bbe07b88f36be2dbe8d55f26146766fbc37f
lembarek <lembarek2011@gmail.com>
2016-09-23 19:49:12 +0100

    WIP, toggle sidebar

--- src/assets/js/acp/admin/sidebar.js
+++ src/assets/js/acp/admin/sidebar.js
@@ -6,3 +6,5 @@ $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
 $('ul.nav a').filter(function() {
     return this.href == url;
 }).parent().addClass('active');
+
+Vue.transition('sidebar', {});
